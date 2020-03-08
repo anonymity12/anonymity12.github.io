c9375b555987aa8d160fb0ee73bc2b61e829bfc5
paul <ztt1995120@gmail.com>
2020-03-08 21:06:57 +0800

    push src md files

--- commitPublish.sh
+++ commitPublish.sh
@@ -2,8 +2,13 @@
 printf "%s" `date`
 cd ~/code/anonymity12.github.io
 git pull
+echo "pulling..."
 hexo clean
+hexo generate
+echo "genertating..."
 hexo deploy
+echo "deploying to master"
 git add .
 git commit -m "push src md files"
 git push
+echo "pushing to hexo branch"
