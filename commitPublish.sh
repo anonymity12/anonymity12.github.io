8b999484751b94ee11bb0ad2b55138372fdd2a3b
paul <ztt1995120@gmail.com>
2020-03-15 19:58:12 +0800

    push src md files

--- commitPublish.sh
+++ commitPublish.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+# 0315 update no more push to github, only local
 printf "%s" `date`
 cd ~/code/anonymity12.github.io
-git pull
-echo "pulling..."
+#git pull
+#echo "pulling..."
 hexo clean
 hexo generate
 echo "genertating..."
-hexo deploy
-echo "deploying to master"
+#hexo deploy
+#echo "deploying to master"
 git add .
 git commit -m "push src md files"
-git push
-echo "pushing to hexo branch"
+hexo s
+#git push
+#echo "pushing to hexo branch"
